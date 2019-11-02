c06348a8aa456245a15ab579a29e2acecece516c
CruizK <cruiz.kusman@gmail.com>
2019-11-02 10:40:49 -0500

    Functional Chat that works between instances

--- server/socket.js
+++ server/socket.js
@@ -1,10 +1,17 @@
 
+
+let users = 0;
 module.exports = (io) => {
   io.on('connection', (socket) => {
+    users++;
+    socket.emit('setName', "Player" + users);
     console.log("There is a connection");
-
+    socket.on('message', (msg) => {
+      io.emit('message', msg.name + ": " + msg.msg);
+    })
     socket.on('disconnect', () => {
       console.log("Disconnection");
+      users--;
     })
   })
 }
