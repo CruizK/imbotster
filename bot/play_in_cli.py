74199fcf5f2069c9bff736bfbb9caf911d42ec38
Sai Shanmukha <nss10@outlook.com>
2019-11-03 11:24:39 -0600

    Added the parser to get training data and update training data

--- bot/play_in_cli.py
+++ bot/play_in_cli.py
@@ -1,22 +1,28 @@
 from webscraper import get_response
-f = open("questionyquestions")
-qno = -1
+from ymlHandler import write_qDict
+from ymlHandler import generate_qDict
 
 qlist=[]
+qDict=generate_qDict()
+i=0
+for question in qDict:
+    qlist.append(question)
+    print(i+1,":",question)
+    i=i+1
 
-for i,statement in enumerate(f):
-  qlist.append(statement.replace("\n",""))
-  print(i,":",statement)
 print("\n\nChoose a menu option: ")
 
 Tags = ['name', 'beautiful-colours', 'countries', 'top-ten-dream-jobs', 'season', 'best-movies', 'best-countries-take-holiday', 'overrated-jobs', 'fast-food-holiday-menu-items', 'best-restaurants', 'best-digital-camera-brands', 'best-social-media-sites', 'songs', 'popular-us-states', 'old', 'how-are-you', 'surprise-me', 'sup', 'likes', 'favorite-pizza-toppings', 'most-popular-celebrities']
-
 while True:
   try:
       qno = int(input())
-      print(qlist[qno])
-      ans = get_response(Tags[qno])
-      print(ans)
+      if(qno==-1):
+          write_qDict(qDict)
+          break
+      print(qlist[qno-1])
+      ans = input()
+      qDict[qlist[qno-1]].append(ans)
+      print(qDict[qlist[qno-1]])
       print("Enter another question number. Or Ctrl C to exit")
   # Press ctrl-c or ctrl-d on the keyboard to exit
   except (KeyboardInterrupt, EOFError, SystemExit):
